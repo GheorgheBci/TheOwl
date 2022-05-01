2947ac09f4d6ed5368b1a9db61d616064d13e452
GheorgheBci <georgebucu96@gmail.com>
2022-05-01 22:01:47 +0200

    Añadir el metodo cargarImagenUsuario al controlador UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsuarioController extends Controller
 {
     public function homeUser()
     {
         return view('auth.userAccount');
+    }
+
+    public function cargarImagenUsuario(Request $request)
+    {
+        if ($request->hasFile('file')) {
+            $imagen = $request->file;
+
+            $imagen->move(public_path() . '/imagenes', $imagen->getClientOriginalName());
+
+            Usuario::where('codUsu', Auth::user()->codUsu)->update([
+                'imagen_usuario' => $imagen->getClientOriginalName()
+            ]);
+        }
     }
 }
