661132ad91587d13a43c2c5ce2ac594eb5d479a7
GheorgheBci <georgebucu96@gmail.com>
2022-05-04 22:30:28 +0200

    Eliminar el campo idRol de la función create en la clase RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,7 +75,6 @@ class RegisterController extends Controller
             'fecNacimiento' => $data['fechaNac'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'idRol' => 1
         ]);
     }
 }
