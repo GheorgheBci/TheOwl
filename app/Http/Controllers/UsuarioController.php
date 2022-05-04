048b4590f833df0f11d8b90162ddc7e136490286
GheorgheBci <georgebucu96@gmail.com>
2022-05-04 22:36:01 +0200

    Añadir nuevas funciones al controlador UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
@@ -24,5 +25,80 @@ class UsuarioController extends Controller
                 'imagen_usuario' => $imagen->getClientOriginalName()
             ]);
         }
+
+        return redirect()->route('usuario.userHome');
+    }
+
+    public function actualizarDatosPersonales(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'string|max:20',
+            'ape1' => 'string|max:20',
+            'ape2' => 'string|max:35|nullable',
+            'fechaNac' => 'date',
+            'email' => 'string|email|max:255|unique:usuario',
+        ]);
+
+        Usuario::where('codUsu', Auth::user()->codUsu)->update([
+            'nombre' => $request->nombre,
+            'apellido1' => $request->ape1,
+            'apellido2' => $request->ape2,
+            'email' => $request->email,
+            'fecNacimiento' => $request->fecNac
+        ]);
+
+        return redirect()->route('usuario.userHome');
+    }
+
+    public function cambiarContraseña(Request $request)
+    {
+        if (Hash::check($request->password, Auth::user()->password)) {
+
+            $request->validate([
+                'password' => 'required|string|min:8',
+                'newPassword' => 'required|string|min:8',
+                'password-confirm' => 'required|min:8|same:newPassword',
+            ]);
+
+            Usuario::where('codUsu', Auth::user()->codUsu)->update([
+                'password' => Hash::make($request->newPassword),
+            ]);
+        }
+
+        return redirect()->route('usuario.userHome');
+    }
+
+    public function socio(Request $request)
+    {
+        date_default_timezone_set('Europe/Madrid');
+
+        switch ($request->tipo) {
+            case "1":
+                $date_future = date("Y-m-d", strtotime('+30 day', strtotime(date("Y-m-d"))));
+                break;
+            case "6":
+                $date_future = date("Y-m-d", strtotime('+180 day', strtotime(date("Y-m-d"))));
+                break;
+            case "12":
+                $date_future = date("Y-m-d", strtotime('+365 day', strtotime(date("Y-m-d"))));
+                break;
+        }
+
+        Usuario::where('codUsu', Auth::user()->codUsu)->update([
+            'idRol' => 2,
+            'fec_ini_socio' => date("Y-m-d"),
+            'fec_fin_socio' => $date_future,
+        ]);
+
+        return redirect()->route('membresia');
+    }
+
+    public function bajaSocio()
+    {
+        Usuario::where('codUsu', Auth::user()->codUsu)->update([
+            'baja' => 1
+        ]);
+
+        return redirect()->route('usuario.userHome');
     }
 }
