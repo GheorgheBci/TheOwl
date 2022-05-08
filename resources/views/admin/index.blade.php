fd8fc5ad391f9bdc8e56f2fb7728a560ac77688c
GheorgheBci <georgebucu96@gmail.com>
2022-05-08 20:36:57 +0200

    Subir la vista principal del administrador

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('titulo', 'Inicio')
+
+@section('content')
+    <div class="ultimosAlquileres">
+    </div>
+
+    <div class="beneficios">
+
+    </div>
+
+    <div class="numeroAlquileres">
+
+    </div>
+@endsection
