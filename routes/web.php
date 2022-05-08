f420de045e4bcc0ea0733a733e6a7ac132ede9ee
GheorgheBci <georgebucu96@gmail.com>
2022-05-08 20:41:32 +0200

    Actualizar el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\EjemplarController;
+use App\Http\Controllers\EditorialController;
+use App\Http\Controllers\ColeccionController;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\AlquilerController;
 use Illuminate\Support\Facades\Route;
-use PHPUnit\Framework\Constraint\LogicalOr;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +43,41 @@ Route::group(['usuario' => 'usuario', 'as' => 'usuario.', 'middleware' => ['auth
     Route::post('actualizarContraseña', [UsuarioController::class, 'cambiarContraseña'])->name('actualizarContraseña');
     Route::get('comprar/{tipo}', [UsuarioController::class, 'socio'])->name('comprar');
     Route::get('baja', [UsuarioController::class, 'bajaSocio'])->name('baja');
+
+    Route::get('usuarios', [UsuarioController::class, 'usuarios'])->name('usuarios');
+    Route::get('eliminar/{usuario}', [UsuarioController::class, 'eliminarCuenta'])->name('eliminar');
+    Route::post('buscarUsuario', [UsuarioController::class, 'buscarUsuario'])->name('buscar');
+});
+
+Route::group(['rol' => 'rol', 'as' => 'rol.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('roles', [RolController::class, 'roles'])->name('roles');
+});
+
+Route::group(['ejemplar' => 'ejemplar', 'as' => 'ejemplar.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('ejemplares', [EjemplarController::class, 'ejemplares'])->name('ejemplares');
+    Route::post('buscar', [EjemplarController::class, 'buscarEjemplar'])->name('buscar');
+});
+
+Route::group(['editorial' => 'editorial', 'as' => 'editorial.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('editoriales', [EditorialController::class, 'editoriales'])->name('editoriales');
+});
+
+Route::group(['coleccion' => 'coleccion', 'as' => 'coleccion.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('colecciones', [ColeccionController::class, 'colecciones'])->name('colecciones');
+});
+
+Route::group(['autor' => 'autor', 'as' => 'autor.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('autores', [AutorController::class, 'autores'])->name('autores');
+});
+
+Route::group(['alquiler' => 'alquiler', 'as' => 'alquiler.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('alquileres', [AlquilerController::class, 'alquileres'])->name('alquileres');
 });
 
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin', function () {
+    return view('admin.index');
+})->name('admin');
