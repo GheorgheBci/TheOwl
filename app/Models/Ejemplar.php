e1a9653ca9196187b1732d5516fbe5061024c791
GheorgheBci <georgebucu96@gmail.com>
2022-05-08 20:44:22 +0200

    Añadir las funciones coleccion, autor y editorial al modelo Ejemplar

--- app/Models/Ejemplar.php
+++ app/Models/Ejemplar.php
@@ -22,4 +22,19 @@ class Ejemplar extends Model
         'codAutor',
         'codColeccion'
     ];
+
+    public function coleccion()
+    {
+        return $this->belongsTo('App\Models\Coleccion', 'codColeccion')->get();
+    }
+
+    public function autor()
+    {
+        return $this->belongsTo('App\Models\Autor', 'codAutor')->get();
+    }
+
+    public function editorial()
+    {
+        return $this->belongsTo('App\Models\Editorial', 'codEditorial')->get();
+    }
 }
