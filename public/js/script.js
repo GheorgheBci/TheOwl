c11ea781404c580ea8995298ca9dad8fe922ed72
GheorgheBci <georgebucu96@gmail.com>
2022-05-12 22:15:13 +0200

    Actualización del script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,63 @@
 
 let activo = false;
 
-document.getElementById('u').addEventListener('click', function () {
+var boton = document.getElementById("menu_barra");
+
+boton.addEventListener("click", function () {
 
     if (!activo) {
-        document.getElementById("menu_desplegable_usuario").style.visibility = 'visible';
-        document.getElementById("flecha").style.visibility = 'visible';
+        anime({
+            targets: '#menu_barra',
+            translateX: 150,
+            duration: 1000,
+            easing: 'linear'
+        });
+        anime({
+            targets: '#c',
+            translateX: 150,
+            duration: 1000,
+            easing: 'linear'
+        });
+
         activo = true;
+
     } else {
-        document.getElementById("menu_desplegable_usuario").style.visibility = 'hidden';
-        document.getElementById("flecha").style.visibility = 'hidden';
+        anime({
+            targets: '#menu_barra',
+            translateX: 0,
+            duration: 1000,
+            easing: 'linear'
+        });
+        anime({
+            targets: '#c',
+            translateX: 0,
+            duration: 1000,
+            easing: 'linear'
+        });
+
         activo = false;
     }
+})
 
-});
+document.getElementById('menu_usuario').addEventListener("click", function () {
+    if (!activo) {
+        anime({
+            targets: '.menu_user_account',
+            translateX: 150,
+            duration: 1000,
+            easing: 'linear'
+        });
+
+        activo = true;
+
+    } else {
+        anime({
+            targets: '.menu_user_account',
+            translateX: 0,
+            duration: 1000,
+            easing: 'linear'
+        });
+
+        activo = false;
+    }
+})
