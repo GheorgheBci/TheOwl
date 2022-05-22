e306dfda9423cd7bf9782817fa243b82239b2016
GheorgheBci <georgebucu96@gmail.com>
2022-05-22 20:55:48 +0200

    Actualización del script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,63 +2,93 @@
 
 let activo = false;
 
-var boton = document.getElementById("menu-barra");
+const boton = document.getElementById("menu-barra");
+const menu_usuario = document.getElementById("menu_usuario");
+const ordenar = document.getElementById('mostrar-ordenar');
 
-boton.addEventListener("click", function () {
+let puntuacion = document.getElementById('puntuacion');
 
-    if (!activo) {
-        anime({
-            targets: '#menu-barra',
-            translateX: 150,
-            duration: 200,
-            easing: 'linear'
-        });
-        anime({
-            targets: '#c',
-            translateX: 150,
-            duration: 200,
-            easing: 'linear'
-        });
+if (puntuacion) {
+    const estrellas = document.getElementsByClassName('puntuacion__a');
 
-        activo = true;
-
-    } else {
-        anime({
-            targets: '#menu-barra',
-            translateX: 0,
-            duration: 200,
-            easing: 'linear'
-        });
-        anime({
-            targets: '#c',
-            translateX: 0,
-            duration: 200,
-            easing: 'linear'
-        });
-
-        activo = false;
+    for (let i = 0; i < puntuacion.innerHTML; i++) {
+        estrellas[i].style.color = "rgb(228, 228, 40)";
     }
-})
+}
 
-document.getElementById('menu_usuario').addEventListener("click", function () {
-    if (!activo) {
-        anime({
-            targets: '.menu_user_account',
-            translateX: 150,
-            duration: 1000,
-            easing: 'linear'
-        });
+if (ordenar) {
+    ordenar.addEventListener('click', function () {
 
-        activo = true;
+        if (!activo) {
+            document.getElementById('opciones-ordenar').style.visibility = "visible";
+            activo = true;
+        } else {
+            document.getElementById('opciones-ordenar').style.visibility = "hidden";
+            activo = false;
+        }
+    });
+}
 
-    } else {
-        anime({
-            targets: '.menu_user_account',
-            translateX: 0,
-            duration: 1000,
-            easing: 'linear'
-        });
+if (boton) {
+    boton.addEventListener("click", function () {
 
-        activo = false;
-    }
-})
+        if (!activo) {
+            anime({
+                targets: '#menu-barra',
+                translateX: 150,
+                duration: 200,
+                easing: 'linear'
+            });
+            anime({
+                targets: '#c',
+                translateX: 150,
+                duration: 200,
+                easing: 'linear'
+            });
+
+            activo = true;
+
+        } else {
+            anime({
+                targets: '#menu-barra',
+                translateX: 0,
+                duration: 200,
+                easing: 'linear'
+            });
+            anime({
+                targets: '#c',
+                translateX: 0,
+                duration: 200,
+                easing: 'linear'
+            });
+
+            activo = false;
+        }
+    })
+}
+
+if (menu_usuario) {
+    document.getElementById('menu_usuario').addEventListener("click", function () {
+        if (!activo) {
+            anime({
+                targets: '.menu_user_account',
+                translateX: 150,
+                duration: 1000,
+                easing: 'linear'
+            });
+
+            activo = true;
+
+        } else {
+            anime({
+                targets: '.menu_user_account',
+                translateX: 0,
+                duration: 1000,
+                easing: 'linear'
+            });
+
+            activo = false;
+        }
+    })
+
+}
