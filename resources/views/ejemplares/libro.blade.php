bcbdb8eecfc009d6304265f0110e44367995adf5
GheorgheBci <georgebucu96@gmail.com>
2022-06-05 21:04:38 +0200

    Añadir iconos en el menu de leer un ejemplar

--- resources/views/ejemplares/libro.blade.php
+++ resources/views/ejemplares/libro.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
     <script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
     <script src="{{ asset('js/pdf.js') }}" async></script>
+    <script src="https://kit.fontawesome.com/3be00db212.js" crossorigin="anonymous"></script>
     <title>Libro</title>
 </head>
 
@@ -21,12 +22,17 @@
     <div class="menu__div--sticky">
         <div class="menu__botones">
             <input type="hidden" id="url" value="{{ $ejemplar->contenido }}">
-            <button id="anterior-pagina" class="menu__boton">Anterior</button>
-            <button id="siguiente-pagina" class="menu__boton">Siguiente</button>
-            <button id="acercar" class="menu__boton">Acercar</button>
-            <button id="alejar" class="menu__boton">Alejar</button>
-            <button id="default" class="menu__boton">100%</button>
-            <button id="cambiar-color-fondo" class="menu__boton">Oscuro</button>
+            <button id="anterior-pagina" title="Anterior" class="menu__boton"><i
+                    class="fa-solid fa-arrow-left"></i></button>
+            <button id="siguiente-pagina" title="Siguiente" class="menu__boton"><i
+                    class="fa-solid fa-arrow-right"></i></button>
+            <button id="acercar" title="Acercar" class="menu__boton"><i
+                    class="fa-solid fa-magnifying-glass-plus"></i></button>
+            <button id="alejar" title="Alejar" class="menu__boton"><i
+                    class="fa-solid fa-magnifying-glass-minus"></i></button>
+            <button id="default" title="Reset Zoom" class="menu__boton">100%</button>
+            <button id="cambiar-color-fondo" title="Modo Oscuro" class="menu__boton"><i id="ico"
+                    class="fa-solid fa-moon"></i></button>
         </div>
 
         <div class="menu__pagina">
